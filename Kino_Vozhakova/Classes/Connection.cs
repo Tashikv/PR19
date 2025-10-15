1be6e7571fb7ece83716938b021ddfbe5b67070f
Tashikv <vozhakovatasha1@gmail.com>
2025-10-15 09:13:18 +0500

    Метод подключения к БД. Вожакова

---
+++ Kino_Vozhakova/Classes/Connection.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino_Vozhakova.Classes
+{
+    public class Connection
+    {
+        private static readonly string connectionString = "server=127.0.0.1;port=3306;database=Kino;uid=root;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        public static MySqlDataReader Query(string sql, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            return command.ExecuteReader();
+        }
+
+        public static int ExecuteNonQuery(string sql, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            return command.ExecuteNonQuery();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            if (connection != null)
+            {
+                connection.Close();
+                MySqlConnection.ClearPool(connection);
+            }
+        }
+    }
+}
